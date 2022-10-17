f6c8019832b8c9bfb52d7908c94c64776977c563
Abdul Haidar <abdulhdr1@gmail.com>
2022-10-16 22:08:01 -0300

    feat(favicon): add favicon

--- src/root.tsx
+++ src/root.tsx
@@ -19,9 +19,10 @@ export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>SolidStart - With TailwindCSS</Title>
+        <Title>Abdul Haidar</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="shortcut icon" type="image/ico" href="/public/favicon.ico" />
       </Head>
       <Body>
         <Suspense>
